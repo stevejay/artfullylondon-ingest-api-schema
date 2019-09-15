0ed11058610d1811c8bee6fb12afddde18b5cfb3
Steve Johns <steve@stevejay.net>
2019-09-15 15:40:54 +0100

    create separate file for version

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,19 @@
+from os import path
 from setuptools import setup
 
+THIS_DIR = path.dirname(__file__)
+VERSION_FILE = path.join(THIS_DIR, '../VERSION')
+
+version_file = open(VERSION_FILE)
+version = version_file.read().strip()
 
 setup(
     name='artfullylondon_ingestschema',
 
-    version='0.2.2',
+    version=version,
     python_requires='>=3.7',
 
-    description='',
+    description='Domain schema for Artfully London ingest system',
     long_description='',
 
     author='Steve Johns',
